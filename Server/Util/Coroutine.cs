1a8a789be0f13fd66a242609a4f732c5a9ce7b53
Jonathan Albert <47063257+jalbert-dev@users.noreply.github.com>
2020-07-02 05:48:12 -0400

    Coroutine docpass

--- Server/Util/Coroutine.cs
+++ Server/Util/Coroutine.cs
@@ -7,6 +7,10 @@ namespace Util
 {
     public static class Coroutines
     {
+        /// <summary>
+        /// Creates an enumerable that waits for the given number of updates
+        /// before returning.
+        /// </summary>
         public static IEnumerable WaitForFrames(int frameCount)
         {
             for (; frameCount >= 0; frameCount--)
@@ -14,6 +18,11 @@ namespace Util
             yield break;
         }
 
+        /// <summary>
+        /// Creates an enumerable from an asynchronous Task. When the Task
+        /// completes, the enumerable will return.
+        /// </summary>
+        /// <param name="task">A task to watch.</param>
         public static IEnumerable WaitForTask(Task task)
         {
             while (!task.IsCompleted)
@@ -22,6 +31,14 @@ namespace Util
         }
     }
 
+    /// <summary>
+    /// A Coroutine object represents a stack of enumerables, where only the 
+    /// topmost enumerable may be executed, and must complete to be removed
+    /// from the stack. Yielding another enumerable from inside the topmost
+    /// enumerable will push it to this stack.
+    /// 
+    /// Coroutines are deemed 'done' when the callstack is empty.
+    /// </summary>
     public class Coroutine
     {
         private const int INITIAL_STACK_SIZE = 8;
@@ -75,21 +92,36 @@ namespace Util
 
     public class CoroutineContainer
     {
-
         List<Coroutine> coroutines = new List<Coroutine>();
 
-        public void Start(IEnumerable co) 
-        {
-            Add(co);
-            coroutines.Last().Step();
-        }
+        /// <summary>
+        /// Creates a coroutine for the given enumerable and adds it to the
+        /// CoroutineContainer. This method does not begin execution of the
+        /// created coroutine.
+        /// </summary>
         public void Add(IEnumerable co)
         {
             var coroutine = new Coroutine(co);
             coroutines.Add(coroutine);
         }
+        /// <summary>
+        /// Creates a coroutine, adds it to the CoroutineContainer, and
+        /// immediately executes it once.
+        /// </summary>
+        public void AddAndExecute(IEnumerable co) 
+        {
+            Add(co);
+            coroutines.Last().Step();
+        }
+        /// <summary>
+        /// Removes all coroutines from the CoroutineContainer.
+        /// </summary>
         public void ClearAll() => coroutines.Clear();
 
+        /// <summary>
+        /// Executes every coroutine in the CoroutineContainer once,
+        /// after which any finished coroutine is removed from the container.
+        /// </summary>
         public void Update()
         {
             coroutines.RemoveAll(coroutine => {
