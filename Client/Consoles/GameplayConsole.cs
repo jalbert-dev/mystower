6a27b03f06e593df0016b973b7b1599d7bf37625
Jonathan Albert <47063257+jalbert-dev@users.noreply.github.com>
2020-07-25 21:52:02 -0400

    Remove duplicate ActorSet (???)

--- Client/Consoles/GameplayConsole.cs
+++ Client/Consoles/GameplayConsole.cs
@@ -17,39 +17,6 @@ namespace Client
     {
         public class Gameplay : SadConsole.Console, IResizeHandler
         {
-            public class ActorSet
-            {
-                private List<MapActor> MapActors { get; } = new List<MapActor>(64);
-                public IEnumerable<MapActor> Actors => MapActors;
-
-                public void Remove(DataHandle<Actor> actor)
-                {
-                    MapActors.RemoveAll(x => {
-                        var shouldRemove = x.Actor.HandleEquals(actor);
-                        if (shouldRemove)
-                            OnRemoveActor?.Invoke(x);
-                        return shouldRemove;
-                    });
-                }
-
-                public MapActor? Lookup(DataHandle<Actor> actor)
-                    => MapActors.FirstOrDefault(x => x.Actor.HandleEquals(actor));
-
-                public void Add(MapActor mapActor)
-                {
-                    if (MapActors.Any(x => x.Actor.HandleEquals(mapActor.Actor)))
-                        return;
-                    OnAddActor?.Invoke(mapActor);
-                    MapActors.Add(mapActor);
-                }
-
-                public MapActor? this[DataHandle<Actor> index] => Lookup(index);
-
-                public delegate void ActorSetHandler(MapActor actor);
-                public event ActorSetHandler? OnRemoveActor;
-                public event ActorSetHandler? OnAddActor;
-            }
-
             public Consoles.TileMap TileMap { get; }
             public Consoles.DebugStats DebugStatsDisplay { get; }
             private readonly Consoles.MessageLog messageLogConsole;
